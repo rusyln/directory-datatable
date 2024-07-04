8a3f75d6a692aae46a7974844b1c0f8b3dd29bbe
Rus <naritorusyl@gmail.com>
2024-07-04 11:29:55 +0800

    Fix class

--- js/initialize-datatable.js
+++ js/initialize-datatable.js
@@ -14,6 +14,15 @@
               { data: "offices" },
               { data: "position" },
             ],
+            dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+                 "<'row'<'col-sm-12'tr>>" +
+                 "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            order: [[0, 'asc']],
+            pagingType: "bootstrap",
+            language: {
+              search: "_INPUT_",
+              searchPlaceholder: "Search records"
+            }
           });
         });
       }
